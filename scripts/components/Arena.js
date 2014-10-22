d54bae5681823f51165bcbaf6f5b3c79cb03d50d
Bradley Anderson <banderso@nerdery.com>
2014-10-21 21:28:59 -0500

    Adding starting positions to Arena.

--- scripts/components/Arena.js
+++ scripts/components/Arena.js
@@ -2,16 +2,36 @@ define(function (require) {
     'use strict';
 
     var Grid = require('components/Grid');
+    var Position = require('models/Position');
 
 
     function Arena () {
 
         this.element = document.createElement('div');
 
-        this.grid = new Grid(12, 12);
+        this.grid = new Grid(Arena.UNITS_WIDE, Arena.UNITS_TALL);
 
         this.init();
     }
+
+
+    Arena.UNITS_WIDE = 12;
+
+    Arena.UNITS_TALL = 12;
+
+
+    Arena.startingPositions = [
+        [
+            new Position(0, 0),
+            new Position(1, 0),
+            new Position(0, 1)
+        ],
+        [
+            new Position(Arena.UNITS_WIDE - 1, Arena.UNITS_TALL - 1),
+            new Position(Arena.UNITS_WIDE - 2, Arena.UNITS_TALL - 1),
+            new Position(Arena.UNITS_WIDE - 1, Arena.UNITS_TALL - 2)
+        ]
+    ];
 
 
     Arena.prototype.init = function () {
---
+++ scripts/models/Position.js
@@ -0,0 +1,15 @@
+define(function (require) {
+    'use strict';
+
+
+    function Position (col, row) {
+
+        this.col = col;
+
+        this.row = row;
+
+    }
+
+
+    return Position;
+});
