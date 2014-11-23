8536dcdb0be26a877057ee734e5b5ff375037e80
Bradley Anderson <banderso@nerdery.com>
2014-11-23 14:08:35 -0600

    Adding jQuery and velocity to so we can start doing programmatic, complex animations.

--- src/assets/scripts/config.js
+++ src/assets/scripts/config.js
@@ -1,7 +1,8 @@
 require.config({
-
     paths: {
-        bluebird: '../vendor/bluebird/js/browser/bluebird'
+        bluebird: '../vendor/bluebird/js/browser/bluebird',
+        jquery: '../vendor/jquery/dist/jquery',
+        requirejs: '../vendor/requirejs/require',
+        velocity: '../vendor/velocity/velocity'
     }
-
 });
