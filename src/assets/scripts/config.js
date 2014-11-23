078cbff1303627490d2333ec287e7e64172ea4d9
Bradley Anderson <banderso@nerdery.com>
2014-11-23 16:21:04 -0600

    Adding jQuery as a dependency for velocity in the config.

--- src/assets/scripts/config.js
+++ src/assets/scripts/config.js
@@ -4,5 +4,12 @@ require.config({
         jquery: '../vendor/jquery/dist/jquery',
         requirejs: '../vendor/requirejs/require',
         velocity: '../vendor/velocity/velocity'
+    },
+    shim: {
+        velocity: {
+            deps: [
+                'jquery'
+            ]
+        }
     }
 });
